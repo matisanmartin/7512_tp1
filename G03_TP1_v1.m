01ae14dd9538f5f9307be3975b40da0d5aa5a29f
Matias San Martin Acosta <msanmartinacosta@gmail.com>
2017-09-15 20:25:01 -0300

    Agregado calculo de error relativo y absoluto entre resultados

--- G03_TP1_v1.m
+++ G03_TP1_v1.m
@@ -1,7 +1,11 @@
 # 75.12 - Análisis Numerico 1
 # San Martin Acosta, Matias
 # Serra, Camila
-format long; # Activa doble precision
+
+# Activa doble precision
+format long; 
+
+# Valor de x de acuerdo a enunciado
 x=1;
 
 # Declaración de funciones anonimas
@@ -26,7 +30,7 @@ error_n_8_a = calcular_error_estimado(8,x,errf_a, factor_a)
 error_n_8_b = calcular_error_estimado(8,x,errf_b, factor_b)
 
 
-# Cuadratura de gauss
+# Cuadratura de gauss, tabla de valores de Burden
 a_2 = [0.5773502692, -0.5773502692];
 a_3 = [0.7745966692, 0.0000000000, -0.7745966692];
 a_4 = [0.8611363116, 0.3399810436, -0.3399810436, -0.8611363116];
@@ -37,10 +41,52 @@ c_3 = [0.5555555556, 0.8888888889, 0.5555555556];
 c_4 = [0.3478548451, 0.6521451549, 0.6521451549, 0.3478548451];
 c_5 = [0.2369268850, 0.4786286705, 0.5688888889, 0.4786286705, 0.2369268850];
 
-
+# Calculo
 error_n_2_cuad_gauss = calcular_cuadratura_gauss(2,x,c_2,a_2)
 error_n_3_cuad_gauss = calcular_cuadratura_gauss(3,x,c_3,a_3)
 error_n_4_cuad_gauss = calcular_cuadratura_gauss(4,x,c_4,a_4)
 error_n_5_cuad_gauss = calcular_cuadratura_gauss(5,x,c_5,a_5)
+
+# Calculo de errf(1) de Octave
+erf1=erf(1);
+
+# Funciones para calcular error relativo y erro absoluto
+calc_err_rel = @(xi,xv) abs(xi-xv)/xv;
+calc_err_abs = @(xi,xv) abs(xi-xv);
+
+# Comparativa de errores para errf_a
+# Errores relativos
+err_rel_n_4_a_vs_erf1 = calc_err_rel(error_n_4_a, erf1)
+err_rel_n_6_a_vs_erf1 = calc_err_rel(error_n_6_a, erf1)
+err_rel_n_8_a_vs_erf1 = calc_err_rel(error_n_8_a, erf1)
+# Errores absolutos
+err_abs_n_4_a_vs_erf1 = calc_err_abs(error_n_4_a, erf1)
+err_abs_n_6_a_vs_erf1 = calc_err_abs(error_n_6_a, erf1)
+err_abs_n_8_a_vs_erf1 = calc_err_abs(error_n_8_a, erf1)
+
+# Comparativa de errores para errf_b
+#Errores relativos
+err_rel_n_4_b_vs_erf1 = calc_err_rel(error_n_4_b, erf1)
+err_rel_n_6_b_vs_erf1 = calc_err_rel(error_n_6_b, erf1)
+err_rel_n_8_b_vs_erf1 = calc_err_rel(error_n_8_b, erf1)
+#Errores absolutos
+err_abs_n_4_b_vs_erf1 = calc_err_abs(error_n_4_b, erf1)
+err_abs_n_6_b_vs_erf1 = calc_err_abs(error_n_6_b, erf1)
+err_abs_n_8_b_vs_erf1 = calc_err_abs(error_n_8_b, erf1)
+
+#Comparativa de errores para cuadratura de gauss
+#Errores relativos
+err_rel_n_2_cg_vs_erf1 = calc_err_rel(error_n_2_cuad_gauss, erf1)
+err_rel_n_3_cg_vs_erf1 = calc_err_rel(error_n_3_cuad_gauss, erf1)
+err_rel_n_4_cg_vs_erf1 = calc_err_rel(error_n_4_cuad_gauss, erf1)
+err_rel_n_5_cg_vs_erf1 = calc_err_rel(error_n_5_cuad_gauss, erf1)
+#Errores absolutos
+err_abs_n_2_cg_vs_erf1 = calc_err_abs(error_n_2_cuad_gauss, erf1)
+err_abs_n_3_cg_vs_erf1 = calc_err_abs(error_n_3_cuad_gauss, erf1)
+err_abs_n_4_cg_vs_erf1 = calc_err_abs(error_n_4_cuad_gauss, erf1)
+err_abs_n_5_cg_vs_erf1 = calc_err_abs(error_n_5_cuad_gauss, erf1)
+
+
+
 
 
